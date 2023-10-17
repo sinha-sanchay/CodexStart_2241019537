a82df9210e4c9efea0e603ebddd5bc894150bc3c
Sanchay Sinha <140732561+sinha-sanchay@users.noreply.github.com>
2023-10-17 21:03:23 +0530

    Add files via upload

---
+++ Q9_1070.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Q9_1070 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        if (n>1&&n < 4) {
+            System.out.println("NO SOLUTION");
+        } else {
+            StringBuilder evenStr = new StringBuilder();
+            StringBuilder oddStr = new StringBuilder();
+
+            for (int i = 2; i <= n; i += 2) {
+                evenStr.append(i).append(" ");
+            }
+            System.out.print(evenStr);
+            for (int i = 1; i <= n; i += 2) {
+                oddStr.append(i).append(" ");
+            }
+            System.out.print(oddStr);
+                }
+        }
+}
