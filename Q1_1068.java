be6f2be9acf609534649b71faf5b670c7e46937b
Sanchay Sinha <140732561+sinha-sanchay@users.noreply.github.com>
2023-10-17 10:59:50 +0530

    Create Q1_1068.java

---
+++ Q1_1068.java
@@ -0,0 +1,21 @@
+  // Name: Sanchay Sinha
+  // Reg_no.: 2241019537
+  // PS LINK: https://cses.fi/problemset/task/1068/
+
+import java.util.*;
+public class Q1_1068 {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        long n =s.nextInt();
+        System.out.print(n);
+        while(n>1){
+            if(n%2==0){
+                n/=2;
+            }
+            else {
+                n=n*3+1;
+            }
+            System.out.print(" "+n);
+        }
+    }
+}
